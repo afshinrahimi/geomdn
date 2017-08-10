ccf9dc454a0afcb5690791f76a80ff54c318d8c6
afshinrahimi <afshinrahimi@gmail.com>
2017-08-10 17:17:34 +1000

    lang2loc documented and tested

--- lang2loc.py
+++ lang2loc.py
@@ -6,77 +6,37 @@ then try to visualise borders and regions (e.g. try many lat/lon as input and ge
 in the output and visualise that).
 @author: af
 '''
-import matplotlib as mpl
-import re
-import lasagne_layers
-mpl.use('Agg')
-from matplotlib import ticker
-import matplotlib.pyplot as plt
-from matplotlib.mlab import griddata
-from matplotlib.patches import Polygon as MplPolygon
-import seaborn as sns
-sns.set(style="white")
-import operator
 import argparse
-import sys
-from scipy.spatial import ConvexHull
-import os
-import pdb
-import random
-from data import DataLoader
-import numpy as np
-import sys
-from os import path
-import scipy as sp
-import theano
-import shutil
-import theano.tensor as T
-import lasagne
-from lasagne.regularization import regularize_layer_params_weighted, l2, l1
-from lasagne.regularization import regularize_layer_params
-import theano.sparse as S
-from lasagne.layers import DenseLayer, DropoutLayer
+from collections import OrderedDict
 import logging
-import json
-import codecs
+from os import path
 import pickle
-import gzip
-from collections import OrderedDict, Counter
-from sklearn.preprocessing import normalize
+import random
+import sys
+
 from haversine import haversine
-from _collections import defaultdict
-from scipy import stats
-from mpl_toolkits.basemap import Basemap, cm, maskoceans
-from scipy.interpolate import griddata as gd
-from lasagne_layers import SparseInputDenseLayer, GaussianRBFLayer, DiagonalBivariateGaussianLayer, BivariateGaussianLayer
-from shapely.geometry import MultiPoint, Point, Polygon
-import shapefile
-from utils import short_state_names, stop_words, get_us_city_name
-from sklearn.cluster import KMeans, MiniBatchKMeans
+import lasagne
+from lasagne.regularization import l2, l1
+from sklearn.cluster import MiniBatchKMeans
+import theano
+
+from data import DataLoader
+from lasagne_layers import SparseInputDenseLayer
+import lasagne_layers
+import numpy as np
+import scipy as sp
+import theano.sparse as S
+import theano.tensor as T
+from utils import stop_words
 import utils
+
+
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
 np.random.seed(77)
 random.seed(77)
 
-def geo_eval(y_true, y_pred, U_eval, classLatMedian, classLonMedian, userLocation):
-    assert len(y_pred) == len(U_eval), "#preds: %d, #users: %d" %(len(y_pred), len(U_eval))
-    distances = []
-    for i in range(0, len(y_pred)):
-        user = U_eval[i]
-        location = userLocation[user].split(',')
-        lat, lon = float(location[0]), float(location[1])
-        prediction = str(y_pred[i])
-        lat_pred, lon_pred = classLatMedian[prediction], classLonMedian[prediction]  
-        distance = haversine((lat, lon), (lat_pred, lon_pred))
-        distances.append(distance)
 
-    acc_at_161 = 100 * len([d for d in distances if d < 161]) / float(len(distances))
-
-    #logging.info( "Mean: " + str(int(np.mean(distances))) + " Median: " + str(int(np.median(distances))) + " Acc@161: " + str(int(acc_at_161)))
-    logging.info("Mean %f Median %f acc@161 %f" %(np.mean(distances), np.median(distances), acc_at_161))
-        
-    return np.mean(distances), np.median(distances), acc_at_161
 
 def geo_latlon_eval(U_eval, userLocation, latlon_pred, contour_error_on_map=False):
     distances = []
@@ -91,7 +51,6 @@ def geo_latlon_eval(U_eval, userLocation, latlon_pred, contour_error_on_map=Fals
         distances.append(distance)
     acc_at_161 = 100 * len([d for d in distances if d < 161]) / float(len(distances))
     logging.info( "Mean: " + str(int(np.mean(distances))) + " Median: " + str(int(np.median(distances))) + " Acc@161: " + str(int(acc_at_161)))
-    #logging.info("Mean %f Median %f acc@161 %f" %(np.mean(distances), np.median(distances), acc_at_161))
     if contour_error_on_map:
         coordinates = np.array(real_latlons)
         utils.contour(coordinates, distances, filename='distance_contour_' + str(np.median(distances)))
@@ -141,6 +100,18 @@ class NNModel_lang2loc():
 
 
     def unpack_params(self, output, n_comp):
+        """
+        Given the output of a tanh dense layer with 6 * n_comp size for a batch of input
+        reshape the output and extract the mus, sigmas, corxy and pis of each sample.
+        Apply restrictions on the value of each parameter of mixture of Gaussians so
+        that they fall within the allowed range:
+        mus are latitude/longitude and for US latitude should be between (0, 90)
+        and longitude should be between (-180,0).
+        sigmas should be positive (0, +inf) apply softplus and because we want them to be
+        large enough multiply them by 10 which is an empirically chosen multiplier.
+        pis should be positive and sum to 1 apply softmax
+        corxy should be in (-1, 1): apply softwign 
+        """
         output = output.reshape(shape=(-1, 6, n_comp))
         mus = output[:, 0:2, :]
         mus = np.array([90, -180])[np.newaxis, :, np.newaxis] * T.nnet.sigmoid(mus)
@@ -164,6 +135,23 @@ class NNModel_lang2loc():
 
 
     def nll_loss(self, mus, sigmas, corxy, pis, y_true):
+        """
+        negative log likelihood loss of a 2d y_true coordinate in
+        each of the Gaussians with parameters mus, sigmas, corxy, pis.
+        Note that the mus, sigmas and corxy are shared between all samples
+        and only pis are different for each sample.
+        
+        The formula for negative log likelihood is :
+        \mathcal{L}(y \vert x) = - \log\bigg\{\sum_{k=1}^K \pi_k(x)  \mathcal{N}\big(y \vert \mu_k(x), \Sigma_k(x)\big)\bigg\}
+        
+        The size of pis is n_batch x n_components,
+        the size of mus is n_batch x n_components x 2,
+        the size of sigmas is n_batch x n_components x 2 and
+        the size of corxy is n_batch x n_components.
+        
+        The size of y_true is batch_size x 2.
+        """
+
         Y = y_true[:, :, np.newaxis]
         diff = Y - mus
         diffprod = T.prod(diff, axis=-2)
@@ -196,11 +184,14 @@ class NNModel_lang2loc():
         return loss
     
     def pred(self, mus, sigmas, corxy, pis, prediction_method='mixture'):
-        '''
-        select mus that maximize \sum_{pi_i * prob_i(mu)} if mean_prediction is True
-        else
-        select the component with the highest prob (ignore sum and pis)
-        '''
+        """
+        Given a mixture of Gaussians infer a mu that maximizes the mixture.
+        There are two modes:
+        If prediction_method==mixture then predict one of the mus that maximizes
+        \mathcal{P}(\boldsymbol{x}) = \sum_{k=1}^{K} \pi_k \mathcal{N}(\boldsymbol{x} \vert \boldsymbol{\mu_k}, \Sigma_k)
+        
+        If prediction_method==pi return the mu that has the largest pi.
+        """
         if prediction_method == 'mixture':
             #logging.info('predicting the best mixture mus')
             X = mus[:, :, :, np.newaxis]
@@ -247,23 +238,35 @@ class NNModel_lang2loc():
  
   
             
-    def get_symb_mus(self, mus, sigmas, corxy, pis):
-        #sigmainvs = 1.0 / sigmas
-        #sigmainvprods = sigmainvs[:,:, 0] * sigmainvs[:,:, 1]
-        #sigmas2 = sigmas ** 2
-        #corxy2 = corxy **2
-        #diff2 = diff ** 2
-        #diffsigma = diff2 / sigmas2
-        #diffsigmanorm = np.sum(diffsigma, axis=-1)
-        #z = diffsigmanorm - 2 * corxy * diffprod * sigmainvprods
-        #oneminuscorxy2inv = 1.0 / (1.0 - corxy2)
-        #expterm = np.exp(-0.5 * z * oneminuscorxy2inv)
-        #expterm = 1.0
-        #probs = (0.5 / np.pi) * sigmainvprods * T.sqrt(oneminuscorxy2inv) * expterm
-        #probs = pis * probs
-        preds = T.argmax(pis, axis=1)
-        selected_mus = mus[T.arange(mus.shape[0]),preds,:]
-        return selected_mus
+    def get_symb_mus(self, mus, sigmas, corxy, pis, prediction_method="pi"):
+        """
+        Can be used to train an autoencoder that given location
+        trains a mixture density layer and then outputs the same
+        location
+        symbolycally predict the mu that maximizes the mixture model
+        either based on mixture probability of the component
+        with highest pi, see pred_sharedparams
+        """
+        if prediction_method == "mixture":
+            #sigmainvs = 1.0 / sigmas
+            #sigmainvprods = sigmainvs[:,:, 0] * sigmainvs[:,:, 1]
+            #sigmas2 = sigmas ** 2
+            #corxy2 = corxy **2
+            #diff2 = diff ** 2
+            #diffsigma = diff2 / sigmas2
+            #diffsigmanorm = np.sum(diffsigma, axis=-1)
+            #z = diffsigmanorm - 2 * corxy * diffprod * sigmainvprods
+            #oneminuscorxy2inv = 1.0 / (1.0 - corxy2)
+            #expterm = np.exp(-0.5 * z * oneminuscorxy2inv)
+            #expterm = 1.0
+            #probs = (0.5 / np.pi) * sigmainvprods * T.sqrt(oneminuscorxy2inv) * expterm
+            #probs = pis * probs
+            logging.fatal("not implemented!")
+            sys.exit()
+        elif prediction_method == "pi":
+            preds = T.argmax(pis, axis=1)
+            selected_mus = mus[T.arange(mus.shape[0]),preds,:]
+            return selected_mus
 
 
     
@@ -275,11 +278,9 @@ class NNModel_lang2loc():
         l_in_text = lasagne.layers.InputLayer(shape=(None, self.input_size),
                                          input_var=self.X_sym)
 
-        
 
         if self.drop_out and self.dropout_coef > 0:
             l_in_text = lasagne_layers.SparseInputDropoutLayer(l_in_text, p=self.dropout_coef)
-
 
         
         l_hid_text = SparseInputDenseLayer(l_in_text, num_units=self.hid_size, 
@@ -290,35 +291,30 @@ class NNModel_lang2loc():
         #    l_hid_text = lasagne.layers.dropout(l_hid_text, p=self.dropout_coef)
 
 
-
         self.l_out_gaus = lasagne.layers.DenseLayer(l_hid_text, num_units=self.n_bigaus_comp * 6,
                                           nonlinearity=lasagne.nonlinearities.linear,
                                           W=lasagne.init.GlorotUniform())
             
-            
-        #sq_error_coef = 0.01
+        
         output = lasagne.layers.get_output(self.l_out_gaus, self.X_sym)
         mus, sigmas, corxy, pis = self.unpack_params(output, n_comp=self.n_bigaus_comp)
         loss = self.nll_loss(mus, sigmas, corxy, pis, self.Y_sym)
+        #we can add an autoencoder loss as well
+        #sq_error_coef = 0.01
         #predicted_mu = self.get_symb_mus(mus, sigmas, corxy, pis)
         #loss += lasagne.objectives.squared_error(predicted_mu, self.Y_sym).mean() * sq_error_coef
-        
-
-
-            
   
         if self.regul_coef:
             l1_share_out = 0.5
             l1_share_hid = 0.5
             regul_coef_out, regul_coef_hid = self.regul_coef, self.regul_coef
             logging.info('regul coefficient for output and hidden lasagne_layers is ' + str(self.regul_coef))
-            l1_penalty = lasagne.regularization.regularize_layer_params(self.l_out_gaus, l1) * regul_coef_out * l1_share_out
-            l2_penalty = lasagne.regularization.regularize_layer_params(self.l_out_gaus, l2) * regul_coef_out * (1-l1_share_out)
+            #l1_penalty = lasagne.regularization.regularize_layer_params(self.l_out_gaus, l1) * regul_coef_out * l1_share_out
+            #l2_penalty = lasagne.regularization.regularize_layer_params(self.l_out_gaus, l2) * regul_coef_out * (1-l1_share_out)
             l1_penalty = lasagne.regularization.regularize_layer_params(l_hid_text, l1) * regul_coef_hid * l1_share_hid
             l2_penalty = lasagne.regularization.regularize_layer_params(l_hid_text, l2) * regul_coef_hid * (1-l1_share_hid)
 
-
-            loss = loss + l1_penalty + l2_penalty
+            loss += l1_penalty + l2_penalty
 
 
         
@@ -332,7 +328,10 @@ class NNModel_lang2loc():
      
 
     def build_squarederror_regression(self):
-
+        """
+        used in case we want to build a regression model that predicts 2d location coordinates
+        from input text.
+        """
         self.X_sym = S.csr_matrix(name='inputs', dtype=self.dtype)
         self.Y_sym = T.matrix(name='y_true', dtype=self.dtype)
         self.X_autoencoder_sym = T.matrix(name='x_autoencoder', dtype=self.dtype)
@@ -375,8 +374,8 @@ class NNModel_lang2loc():
             l1_share_hid = 0.5
             regul_coef_out, regul_coef_hid = self.regul_coef, self.regul_coef
             logging.info('regul coefficient for output and hidden lasagne_layers is ' + str(self.regul_coef))
-            l1_penalty = lasagne.regularization.regularize_layer_params(self.l_out, l1) * regul_coef_out * l1_share_out
-            l2_penalty = lasagne.regularization.regularize_layer_params(self.l_out, l2) * regul_coef_out * (1-l1_share_out)
+            #l1_penalty = lasagne.regularization.regularize_layer_params(self.l_out, l1) * regul_coef_out * l1_share_out
+            #l2_penalty = lasagne.regularization.regularize_layer_params(self.l_out, l2) * regul_coef_out * (1-l1_share_out)
             l1_penalty = lasagne.regularization.regularize_layer_params(l_hid_text, l1) * regul_coef_hid * l1_share_hid
             l2_penalty = lasagne.regularization.regularize_layer_params(l_hid_text, l2) * regul_coef_hid * (1-l1_share_hid)
 
@@ -393,6 +392,7 @@ class NNModel_lang2loc():
         
     def set_params(self, params):
         lasagne.layers.set_all_param_values(self.l_out_gaus, params)
+
     def iterate_minibatches(self, inputs, targets, batchsize, shuffle=False):
         assert inputs.shape[0] == targets.shape[0]
         if shuffle:
@@ -416,8 +416,6 @@ class NNModel_lang2loc():
                 return
 
                
-        
-        
         logging.info('training with %d n_epochs and  %d batch_size' %(self.n_epochs, self.batch_size))
         best_params = None
         best_val_loss = sys.maxint
@@ -430,6 +428,7 @@ class NNModel_lang2loc():
                 l_train = self.f_train(x_batch, y_batch)
                 l_trains.append(l_train)
             l_train = np.mean(l_trains)
+            #because dev set is big we can't predict in a row because of memory size
             l_vals = []
             for batch in self.iterate_minibatches(X_dev, Y_dev, self.batch_size, shuffle=False):
                 x_batch, y_batch = batch
@@ -437,7 +436,6 @@ class NNModel_lang2loc():
                 l_vals.append(l_val)
             l_val = np.mean(l_vals)
             #preds = self.predict(X_dev)
-            #pdb.set_trace()
 
             if l_val < best_val_loss:
                 best_val_loss = l_val
@@ -456,9 +454,7 @@ class NNModel_lang2loc():
             lasagne.layers.set_all_param_values(self.l_out, best_params)
         else:
             lasagne.layers.set_all_param_values(self.l_out_gaus, best_params)
-        #logging.info('dumping the model...')
-        #with open(model_file, 'wb') as fout:
-        #    pickle.dump(best_params, fout)
+
                 
     def predict(self, X):
         mus_eval, sigmas_eval, corxy_eval, pis_eval = self.f_predict(X)
@@ -470,14 +466,7 @@ class NNModel_lang2loc():
         output = self.f_predict(X)
         return output
 
-          
 
-
-def get_cluster_centers(input, n_cluster, dtype='float32'):
-    #kmns = KMeans(n_clusters=n_cluster, n_jobs=10)
-    kmns = MiniBatchKMeans(n_clusters=n_cluster, batch_size=1000)
-    kmns.fit(input)
-    return kmns.cluster_centers_.astype(dtype)
     
           
 def load_data(data_home, **kwargs):
@@ -504,12 +493,6 @@ def load_data(data_home, **kwargs):
     X_train = dl.X_train.astype(dtype)
     X_dev = dl.X_dev.astype(dtype)
     X_test = dl.X_test.astype(dtype)
-    #dl.assignClasses()
-    #Y_test = dl.test_classes.astype('int32')
-    #Y_train = dl.train_classes.astype('int32')
-    #Y_dev = dl.dev_classes.astype('int32')
-    #classLatMedian = {str(c):dl.cluster_median[c][0] for c in dl.cluster_median}
-    #classLonMedian = {str(c):dl.cluster_median[c][1] for c in dl.cluster_median}
     classLatMedian, classLonMedian = None, None
     loc_train = np.array([[a[0], a[1]] for a in dl.df_train[['lat', 'lon']].values.tolist()], dtype=dtype)
     loc_dev = np.array([[a[0], a[1]] for a in dl.df_dev[['lat', 'lon']].values.tolist()], dtype=dtype)
@@ -530,6 +513,7 @@ def load_data(data_home, **kwargs):
     
     data = (X_train, Y_train, X_dev, Y_dev, X_test, Y_test, U_train, U_dev, U_test, classLatMedian, classLonMedian, userLocation, loc_train)
     return data
+
 def load_toy_data(n_samples=1000, dtype='float32'):
     print('creating Melbourne toy dataset as an inverse problem.')
     print('There are two (if not more) Melbournes, one in Australia and one in Florida, USA')
@@ -579,8 +563,6 @@ def train(data, **kwargs):
     regul = kwargs.get('regul_coef', 1e-6)
     hid_size = kwargs.get('hidden_size', 200)
     autoencoder = kwargs.get('autoencoder', False)
-    grid_transform = kwargs.get('grid', False)
-    rbf = kwargs.get('rbf', False)
     ncomp = kwargs.get('ncomp', 100)
     dataset_name = kwargs.get('dataset_name')
     batch_size = kwargs.get('batch_size', 200 if dataset_name=='cmu' else 1000)
@@ -590,13 +572,15 @@ def train(data, **kwargs):
     output_size = Y_train.shape[1] if len(Y_train.shape) == 2 else np.max(Y_train) + 1
     logging.info('batch size %d' % batch_size)
     max_down = 20 if dataset_name == 'cmu' else 5 
+    
+    
     model = NNModel_lang2loc(n_epochs=10000, batch_size=batch_size, regul_coef=regul, 
                     input_size=input_size, output_size=output_size, hid_size=hid_size, 
                     drop_out=True, dropout_coef=dropout_coef, early_stopping_max_down=max_down, 
                     input_sparse=True, reload=False, ncomp=ncomp, autoencoder=autoencoder, sqerror=sqerror, dataset_name=dataset_name)
 
     model.fit(X_train, Y_train, X_dev, Y_dev, X_test, Y_test, U_train, U_dev, U_test, userLocation)
-    #save some space before prediction
+    #save some space before prediction by deleting X_train (which is big)
     del X_train
     del Y_train
     if model.sqerror:
