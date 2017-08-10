ca301f8db06e9a6cf3e47c308eda3146e792f051
afshinrahimi <afshinrahimi@gmail.com>
2017-08-10 16:13:34 +1000

    layers documented

--- lasagne_layers.py
+++ lasagne_layers.py
@@ -4,12 +4,7 @@ Created on 3 Feb 2017
 @author: af
 '''
 import numpy as np
-import scipy as sp
-import theano
 import theano.tensor as T
-import lasagne
-from lasagne.regularization import regularize_layer_params_weighted, l2, l1
-from lasagne.regularization import regularize_layer_params
 import theano.sparse as S
 from lasagne.layers import DenseLayer, DropoutLayer, Layer
 from collections import OrderedDict
@@ -18,6 +13,9 @@ from collections import OrderedDict
 These sparse classes are copied from https://github.com/Lasagne/Lasagne/pull/596/commits
 '''
 class SparseInputDenseLayer(DenseLayer):
+    """A dense layer suitable to be placed after sparse input layers
+    copied from https://github.com/Lasagne/Lasagne/pull/596/commits
+    """
     def get_output_for(self, input, **kwargs):
         if not isinstance(input, (S.SparseVariable, S.SparseConstant,
                                   S.sharedvar.SparseTensorSharedVariable)):
@@ -29,6 +27,9 @@ class SparseInputDenseLayer(DenseLayer):
         return self.nonlinearity(activation)
 
 class SparseInputDropoutLayer(DropoutLayer):
+    """A dropout layer suitable to be placed after an input layer with sparse data
+    copied from https://github.com/Lasagne/Lasagne/pull/596/commits
+    """
     def get_output_for(self, input, deterministic=False, **kwargs):
         if not isinstance(input, (S.SparseVariable, S.SparseConstant,
                                   S.sharedvar.SparseTensorSharedVariable)):
@@ -50,46 +51,14 @@ class SparseInputDropoutLayer(DropoutLayer):
 
             return input * self._srng.binomial(input_shape, p=retain_prob,
                                                dtype=input.dtype)
-class SparseConvolutionDenseLayer(DenseLayer):
-    def __init__(self, incoming, H=None, **kwargs):
-        super(SparseConvolutionDenseLayer, self).__init__(incoming, **kwargs)
-        self.H = H
-        #self.H = self.add_param(H, (H.shape[0], H.shape[1]), name='H')
 
-        
-    def get_output_for(self, input, **kwargs):
-        if not isinstance(input, (S.SparseVariable, S.SparseConstant,
-                                  S.sharedvar.SparseTensorSharedVariable)):
-            raise ValueError("Input for this layer must be sparse")
-        
-        activation = S.dot(input, self.W)
-        #do the convolution
-        activation = S.dot(self.H, activation)
-
-        if self.b is not None:
-            activation = activation + self.b.dimshuffle('x', 0)
-        return self.nonlinearity(activation)
-
-class ConvolutionDenseLayer(DenseLayer):
-
-    def __init__(self, incoming, H=None, **kwargs):
-        super(ConvolutionDenseLayer, self).__init__(incoming, **kwargs)
-        self.H = H
-        #self.H = self.add_param(H, (H.shape[0], H.shape[1]), name='H')
-    
-    def get_output_for(self, input, **kwargs):
-        target_indices = kwargs.get('target_indices') 
-        activation = T.dot(input, self.W)
-        #do the convolution
-        activation = S.dot(self.H, activation)
-
-        if self.b is not None:
-            activation = activation + self.b.dimshuffle('x', 0)
-        activation = activation[target_indices, :]
-        return self.nonlinearity(activation)
 
 class GaussianRBFLayer(Layer):
-
+    """
+    Given 2d inputs this layer can return the RBF activation of each input
+    in each of the num_units 2d RBFs. It can easily be extended to inputs
+    with higher/lower dimensionality by changing the mus_init dimensionality.
+    """
     def __init__(self, incoming, num_units, mus=None, **kwargs):
         super(GaussianRBFLayer, self).__init__(incoming, **kwargs)
         #self.H = H
@@ -110,6 +79,10 @@ class GaussianRBFLayer(Layer):
         return (input_shape[0], self.num_units)        
     
     def get_output_for(self, input, **kwargs):
+        """
+        Given n-dimensional input and the current mus, find the RBF activation of
+        each input in each RBF component using RBF(x,mu,sigma) =e^{- \sigma_i(x_i-mu_i)^2 * sigma^2}
+        """
         C = self.mus[np.newaxis, :, :]
         X = input[:, np.newaxis, :]
         difnorm = T.sum((C-X)**2, axis=-1)
@@ -117,11 +90,15 @@ class GaussianRBFLayer(Layer):
         return a
 
 class DiagonalBivariateGaussianLayer(Layer):
-
+    """
+    Diagonal Bivariate Gaussian Activation Layer
+    Given 2d input the output of this layer is the probability
+    of each input in each of the num_units Diagonal Bivariate Gaussians
+    using this formula http://mathworld.wolfram.com/BivariateNormalDistribution.html
+    It is fairly easy to customize the class for higher/lower dimensionality
+    """
     def __init__(self, incoming, num_units, mus=None, **kwargs):
         super(DiagonalBivariateGaussianLayer, self).__init__(incoming, **kwargs)
-        #self.H = H
-        #self.H = self.add_param(H, (H.shape[0], H.shape[1]), name='H')
         self.name = 'DiagonalBivariateGaussianLayer'
         self.num_units = num_units
         if mus is not None:
@@ -138,6 +115,10 @@ class DiagonalBivariateGaussianLayer(Layer):
         return (input_shape[0], self.num_units)        
     
     def get_output_for(self, input, **kwargs):
+        """
+        Given 2d input find the probability of each input in each of num_units
+        Diagonal Gaussians using the formula from http://mathworld.wolfram.com/BivariateNormalDistribution.html
+        """
         #make sure sigma is positive and nonzero softplus(x) (0, +inf)
         sigmas = T.nnet.softplus(self.sigmas)
         sigmainvs = 1.0 / sigmas
@@ -154,11 +135,15 @@ class DiagonalBivariateGaussianLayer(Layer):
         
 
 class BivariateGaussianLayer(Layer):
-
+    """
+    Bivariate Gaussian Activation Layer
+    Given 2d input the output of this layer is the probability
+    of each input in each of the num_units  Bivariate Gaussians
+    using this formula http://mathworld.wolfram.com/BivariateNormalDistribution.html
+    It is fairly easy to customize the class for higher/lower dimensionality
+    """
     def __init__(self, incoming, num_units, mus=None, sigmas=None, corxy=None, **kwargs):
         super(BivariateGaussianLayer, self).__init__(incoming, **kwargs)
-        #self.H = H
-        #self.H = self.add_param(H, (H.shape[0], H.shape[1]), name='H')
         self.name = 'BivariateGaussianLayer'
         self.num_units = num_units
         if mus is not None:
@@ -184,7 +169,16 @@ class BivariateGaussianLayer(Layer):
     def get_output_shape_for(self, input_shape):
         return (input_shape[0], self.num_units)        
     
-    def get_output_for(self, input, **kwargs):
+    def get_output_for(self, input, do_softmax=True, **kwargs):
+        """
+        Return the probability of each input sample in
+        each of the num_units Bivariate Gaussian components.
+        Note that if do_softmax=True a softmas would be applied
+        over those probabilities so that it can be used as a stable
+        representation instead of many very small numbers. 
+        If do_softmax=False the returned probabilites
+        might be very small due to high distance from the means.
+        """
         #make sure sigma is positive and nonzero softplus(x) (0, +inf)
         sigmas = T.nnet.softplus(self.sigmas)
         sigmainvs = 1.0 / sigmas
@@ -207,16 +201,24 @@ class BivariateGaussianLayer(Layer):
         z = diffsigmanorm - 2 * corxy * diffprod * sigmainvprods
         oneminuscorxy2inv = 1.0 / (1.0 - corxy2)
         expterm = -0.5 * z * oneminuscorxy2inv
-        #a trick for numerical reasons: a1 * a2 * exp(b) == exp(log(a1) + log(a2) + b)
+        #a trick for numerical stability reasons: a1 * a2 * exp(b) == exp(log(a1) + log(a2) + b)
         #probs = (0.5 / np.pi) * sigmainvprods * T.sqrt(oneminuscorxy2inv) * T.exp(expterm)    #the term without the logexp trick
         #note a x exp(b) = exp(log(a) + b)
         logprobs = T.log(0.5/np.pi) + T.log(sigmainvprods) + T.log(T.sqrt(oneminuscorxy2inv)) + expterm
         probs = T.exp(logprobs)
-        output = T.nnet.softmax(logprobs)
+        if do_softmax:
+            output = T.nnet.softmax(logprobs)
+        else:
+            output = probs
         return output
 
 class MDNSharedParams(DenseLayer):
-
+    """
+    This is an implementation of Mixture Density Networks Bishop (2005) with
+    the difference that mus and sigmas are shared between all input samples
+    by setting them as the parameters of the layer. Only pis are conditioned
+    on input.
+    """
     def __init__(self, incoming, mus=None, sigmas=None, corxy=None, **kwargs):
         super(MDNSharedParams, self).__init__(incoming, **kwargs)
         #self.H = H
@@ -233,8 +235,12 @@ class MDNSharedParams(DenseLayer):
         else:
             #emnlp submission
             sigmas_init = np.array([10, 10]).astype('float32') * np.abs(np.random.randn(self.num_units, 2).reshape((self.num_units,2))).astype('float32')
-            #later corrections (just tested for loc2lang_old)
-            #sigmas_init = np.random.uniform(low=5, high=10, size=(self.num_units, 2)).astype('float32')
+            #Note that sigma initialization is a very difficult task.
+            #if they're very small, the probabilites in all gaussians will be close
+            #to zero which results in numerical instability and nans/infs.
+            #For each indivisual application the initialization should be
+            #thoroughly customized.
+            #e.g. sigmas_init = np.random.uniform(low=5, high=10, size=(self.num_units, 2)).astype('float32')
         
         if corxy is not None:
             corxy_init = corxy
